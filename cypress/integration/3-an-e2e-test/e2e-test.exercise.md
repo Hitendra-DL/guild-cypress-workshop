18c3d616c83555b0a322138902848b5ae8f8b3c3
Hitendra Tochia <hitendra.tochia@doctorlink.com>
2021-08-17 12:10:08 +0100

    Added e2e test and markdown files

---
+++ cypress/integration/3-an-e2e-test/e2e-test.exercise.md
@@ -0,0 +1,6 @@
+# E2E test
+
+## Exercise
+Using the pre-defined `e2e-test.spec.ts` file
+1. Sign in with Valid credentials and check for success
+2. Attempt to Sign in with invalid credentials and check you have not been signed in
---
+++ cypress/integration/3-an-e2e-test/e2e-test.spec.ts
@@ -0,0 +1,17 @@
+describe("E2E test", () => {
+  beforeEach(() =>{
+    cy.visit("cypress/integration/3-an-e2e-test/index.html");
+  })
+  
+
+  it("should be able to Sign in successfully with valid credentials", () => {
+
+
+  });
+
+  
+  it("should not be able to Sign in with invalid credentials", () => {
+
+
+  });
+});
